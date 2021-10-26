96c8c4ab27cd676baff529028ba19cd4558777a1
Ngtuankhanh <93204710+Ngtuankhanh@users.noreply.github.com>
2021-10-26 21:34:16 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text1: "Mừng cậu quay lại web cua 2 bon mình neee",
+  text2: "Anh có điều này muốn hỏi nee,em nhớ phải trả lời thật lòng nhaa.",
+  text3: "Vợ yêuu anhh nhiều nhiềuu lắm đúng hongg neee",
+  text4: "Minh phai tra loi that longg do nhaaaa:'3 ",
+  text5: "Con lau moi yeu anhh",
+  text6: "Em yêu mình nhất trên đờiii <3",
+  text7: "Noi cho anh ly do minh yeu anh dii:'3",
+  text8: "Gửi cho anhh nee <3",
+  text9: "Bởi vì anh là chồng em, anh luôn luôn dành cả trái tim anh để yêu em nhiều lắm đó nee. Anh thật sự, thật sự, thật sự thương em nhiều nhiều nhiềuuu lắm luôn",
+  text10: "Anh biết mà ^^ Anh Anh yêu cả ba mẹ con nhiều lắm nè ❤️",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Sau này dù có thế nào, bọn mình nhất định sẽ cùng nhau đi hết cả cuộc đời này đó nhaa. Mà vợ cũng đừng ngại thể hiện tình cảm cho anh nhiều lên nhee, cả thế giới của anhh",
   text12: "Okii lunn <3",
 };
 
